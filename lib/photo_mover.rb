92415ed4311cf02d5a22f5e4d529befdb4b8cf19
Tray Torrance <devwork@warrentorrance.com>
2011-12-11 14:40:16 -0800

    EB TT Converts PhotoMover from module to class, takes user_id argument

--- lib/photo_mover.rb
+++ lib/photo_mover.rb
@@ -1,24 +1,34 @@
-module PhotoMover
-  def self.move_photos(user)
+require 'open-uri'
+
+class PhotoMover
+
+  def initialize(user_id)
+    @user = User.find_by_id(user_id)
+  end
+
+  def generate_tarball
     Dir.chdir Rails.root
-    temp_dir = "tmp/exports/#{user.id}"
+    temp_dir = "tmp/exports/#{@user.id.to_s}"
     FileUtils::mkdir_p temp_dir
     Dir.chdir 'tmp/exports'
 
-    photos = user.visible_shareables(Post).where(:author_id => user.person.id, :type => 'Photo')
+    photos = @user.photos
 
-    photos_dir = "#{user.id}/photos"
+    photos_dir = "#{@user.id.to_s}/photos"
     FileUtils::mkdir_p photos_dir
 
     photos.each do |photo|
-      current_photo_location = "#{Rails.root}/public/uploads/images/#{photo.remote_photo_name}"
-      new_photo_location = "#{photos_dir}/#{photo.remote_photo_name}"
-      FileUtils::cp current_photo_location, new_photo_location
+      current_photo_location = photo.url
+      new_photo_location     = "#{photos_dir}/#{photo.remote_photo_name}"
+
+      File.open(new_photo_location, 'w') { |f|
+        OpenURI.open_uri(current_photo_location) { |uri| f.write(uri.read) }
+      }
     end
 
-    `tar c #{user.id} > #{user.id}.tar`
+    `tar c #{@user.id.to_s} > #{@user.id}.tar`
     #system("tar", "c", "#{user.id}",">", "#{user.id}.tar")
-    FileUtils::rm_r "#{user.id.to_s}/", :secure => true, :force => true
+    FileUtils::rm_r "#{@user.id.to_s}/", :secure => true, :force => true
 
     "#{Rails.root}/#{temp_dir}.tar"
   end
